1e9746ad9dccb46d0ffe18eedca1961abd1bf96b
Penupothula <sharmila.penupothula@spglobal.com>
2020-12-01 00:19:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function shadowcmp(options) {
+    let boxes = document.querySelectorAll('.shadowcmp');
+
+    if(options.shadow_type === "big")
+        options.shadow_type = "0px"
+    else
+        options.shadow_type = "15px"   
+        
+    boxes.forEach(box => {
+        box.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+        
+        if(options.padding){
+            box.style.padding = "1em";
+        }
+    })
+}
+
+module.exports.shadowcmp = shadowcmp;
